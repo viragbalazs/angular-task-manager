a512f4fd32f71f89663ea003c19ec3c918d7ded7
viragbalazs <viragbalazs95@gmail.com>
2018-12-13 09:17:43 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {AppModel} from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'task-manager';
+
+  public appModel = new AppModel();
+  
+  constructor() {
+
+  }
+  
+  showAppModelValue() {
+    console.log(this.appModel);
+  }
+
+  public changeChecked(isChecked: Event) {
+    this.appModel.checked = !!isChecked;
+  }
+
 }
+
+
+
---
+++ src/app/app.model.ts
@@ -0,0 +1,12 @@
+
+export class AppModel {
+
+  constructor(
+    public title?: string,
+    public checked: boolean = false,
+    public category?: string,
+    public text?: string
+  ) {
+
+  }
+}
