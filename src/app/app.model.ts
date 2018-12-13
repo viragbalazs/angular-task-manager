317344355280d61b99cb288f3f7995a0372314e0
viragbalazs <viragbalazs95@gmail.com>
2018-12-13 10:31:02 +0100

    Category enum added

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -5,8 +5,20 @@ export class AppModel {
     public title?: string,
     public checked: boolean = false,
     public category?: string,
-    public text?: string
-  ) {
+    public text?: string) {
+
+      //Object.keys(Category);
+
+    /*  if (Category.BILLING === category) {
+        console.log('');
+      } */
 
   }
+}
+
+export enum Category {      // statikus fix adattag,konstans értékeket tudunk felvenni hozzá,mindig u.a a példányt fogja visszaadni (enum)
+    BILLING = 'BILLING',
+    HOUSE = 'HOUSE',
+    SHOPPING = 'SHOPPING',
+    TRAVEL = 'TRAVEL'
 }
